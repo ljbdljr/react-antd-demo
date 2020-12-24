31606e52c5f0492f258ccd92396ff8c1ff47cf6a
ljb <1468404645@qq.com>
2020-12-24 19:46:08 +0800

    分支

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 // import logo from "./logo.svg"
 import "./App.css"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { PageLayout } from "./pages/common"
 
 function App() {
-  return <div></div>
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={PageLayout} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
